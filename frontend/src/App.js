e753209ff8f7a135d9de86652a1551476e152e21
eshwar <eshwarchawda08@gmail.com>
2024-11-24 19:10:54 +0530

    Inital Commit - Frontend with Login Component.

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { TextField, Button } from '@mui/material';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  function handleLogin() {
+    console.log(username);
+    console.log(password);
+  }
+
+  return (
+    <form>
+      <TextField
+        id="filled-basic"
+        label="Username"
+        variant="filled"
+        margin="normal"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <br></br>
+      <TextField
+        id="standard-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        variant="standard"
+        margin="normal"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <br>
+      </br>
+      <br>
+      </br>
+      <Button variant="contained" color="primary" onClick={handleLogin}>
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default LoginPage;
